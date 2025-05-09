7becc061e51751f7cd60823fb718cc4bc140401d
magriker <magken7@gmail.com>
2025-05-10 01:43:15 +0900

    commity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const imgContainer = {
+    height: "500px",
+    width: "500px",
+    position: "relative",
+  };
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold">Hello, Next.js!</h1>
+      <Link href="/admin">Admin</Link>
+      <br />
+      <Link href="/admin/table">table</Link>
+      <div className="w-96 h-96 relative">
+        <Image src="/chair.jpg" fill={true} alt="chair"></Image>
+      </div>
+    </div>
+  );
 }
